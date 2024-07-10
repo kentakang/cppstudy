346161b302c85b190def9f692b5bcc4cbc8c6427
Chan Kang <me@kentakang.com>
2024-07-10 16:51:54 +0900

    feat: add chapter1 hello.cpp

---
+++ modoocode/chapter1/hello.cpp
@@ -0,0 +1,13 @@
+// C++의 경우 헤더 뒤에 h를 붙이지 않아도 됨.
+// iostream은 C++의 표준 입출력 헤더 파일
+#include <iostream>
+
+int main() {
+    /*
+     * std:: -> 네임스페이스
+     * << 는 텍스트를 이어주는 역활을 함
+     */
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
